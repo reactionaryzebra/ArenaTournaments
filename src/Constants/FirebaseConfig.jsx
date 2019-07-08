2133c41368330371389db82a808acf121deb506b
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-07-08 13:23:19 -0700

    Add firebaseConfig

---
+++ src/Constants/FirebaseConfig.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQKJPu3BdL-S0gahE2U6JKgg2_A2NnV38",
+  authDomain: "arena-tournaments.firebaseapp.com",
+  databaseURL: "https://arena-tournaments.firebaseio.com",
+  projectId: "arena-tournaments",
+  storageBucket: "arena-tournaments.appspot.com",
+  messagingSenderId: "397250684958",
+  appId: "1:397250684958:web:4076401790870db3"
+};
+
+export default firebaseConfig;
